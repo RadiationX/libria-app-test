2dc86caf174296cb0194187c812059b893709000
Evgeny Nizamiev <radiationx@yandex.ru>
2020-08-17 13:08:42 +0500

    show app list

--- www/views/app-items.php
+++ www/views/app-items.php
@@ -1,6 +1,69 @@
 <?php
 $tpl = $mustache->loadTemplate('app-item');
 
+// из browscap
+$possiblePlatforms = ['unknown', 'Linux', 'Android', 'Win10', 'Win8.1', 'Win8', 'Win7', 'MacOSX', 'WinVista', 'iOS', 'Xbox OS 10', 'WinPhone10', 'Xbox OS 10 (Mobile View)', 'Win32', 'WinPhone8.1', 'ipadOS', 'Win64', 'Xbox OS', 'Xbox 360'];
+$possibleDeviceTypes = ['unknown', 'Desktop', 'TV Device', 'Mobile Phone', 'Tablet', 'Mobile Device'];
+
+$appsData = [
+    'android_mobile' => [
+        'link' => '/android/',
+        'type' => 'android-mobile',
+        'image' => 'app_android_mobile.png',
+        'icon' => 'ic_android_primary.svg',
+        'name' => 'AniLibria',
+        'platform' => 'Android'
+    ],
+    'android_tv' => [
+        'link' => '/android-tv/',
+        'type' => 'android-mobile',
+        'image' => 'app_android_tv.png',
+        'icon' => 'ic_android_primary.svg',
+        'name' => 'AniLibria',
+        'platform' => 'Android TV'
+    ],
+    'ios' => [
+        'link' => '/ios/',
+        'type' => 'android-mobile',
+        'image' => 'app_ios.png',
+        'icon' => 'ic_apple_primary.svg',
+        'name' => 'AniLibria',
+        'platform' => 'iOS'
+    ],
+    'macos_catalyst' => [
+        'link' => '/catalyst/',
+        'type' => 'android-mobile',
+        'image' => 'app_macos_catalyst.png',
+        'icon' => 'ic_apple_primary.svg',
+        'name' => 'AniLibria Catalyst',
+        'platform' => 'macOS'
+    ],
+    'winten' => [
+        'link' => '/win/',
+        'type' => 'android-mobile',
+        'image' => 'app_winten.png',
+        'icon' => 'ic_windows_primary.svg',
+        'name' => 'AniLibria',
+        'platform' => 'Windows 10'
+    ],
+    'cross_anilibrix' => [
+        'link' => '/anilibrix/',
+        'type' => 'android-mobile',
+        'image' => 'app_cross_anilibrix.png',
+        'icon' => 'ic_macbook_primary.svg',
+        'name' => 'AniLibriX',
+        'platform' => 'PC/Mac/Linux'
+    ],
+    'cross_qt' => [
+        'link' => '/qt/',
+        'type' => 'android-mobile',
+        'image' => 'app_cross_qt.png',
+        'icon' => 'ic_macbook_primary.svg',
+        'name' => 'AniLibria QT',
+        'platform' => 'PC/Mac/Linux'
+    ]
+];
+
 $appTypes = [
     'android_mobile' => [
         'platform' => ['android'],
@@ -58,7 +121,6 @@ $priorities = [
 $browserInfo = get_browser(null, true);
 
 function getClientPlatform($browserInfo) {
-    $possiblePlatforms = ['unknown', 'Linux', 'Android', 'Win10', 'Win8.1', 'Win8', 'Win7', 'MacOSX', 'WinVista', 'iOS', 'Xbox OS 10', 'WinPhone10', 'Xbox OS 10 (Mobile View)', 'Win32', 'WinPhone8.1', 'ipadOS', 'Win64', 'Xbox OS', 'Xbox 360'];
     $platform = $browserInfo['platform'];
 
     if (stripos($platform, 'macos') !== false) {
@@ -70,7 +132,6 @@ function getClientPlatform($browserInfo) {
     if (stripos($platform, 'win') !== false) {
         return 'windows';
     }
-
     if (stripos($platform, 'android') !== false) {
         return 'android';
     }
@@ -82,7 +143,6 @@ function getClientPlatform($browserInfo) {
 }
 
 function getClientType($browserInfo) {
-    $possibleDeviceTypes = ['unknown', 'Desktop', 'TV Device', 'Mobile Phone', 'Tablet', 'Mobile Device'];
     $type = $browserInfo['device_type'];
 
     if (stripos($type, 'desktop') !== false) {
@@ -117,13 +177,12 @@ function getClientApps($appTypes, $browserInfo) {
     return array_keys($filtered);
 }
 
-function sortPopularity($clientApps, $priorities, $browserInfo) {
+function sortByPriority($clientApps, $priorities, $browserInfo) {
     $platform = getClientPlatform($browserInfo);
     $type = getClientType($browserInfo);
     if (array_key_exists($platform, $priorities) && array_key_exists($type, $priorities[$platform])) {
         $order = $priorities[$platform][$type];
         usort($clientApps, function ($a, $b) use ($order) {
-            echo "sort $a, $b <br>";
             foreach ($order as $value) {
                 if ($a == $value) {
                     return 0;
@@ -138,30 +197,24 @@ function sortPopularity($clientApps, $priorities, $browserInfo) {
     return $clientApps;
 }
 
-echo 'platform: ' . getClientPlatform($browserInfo) . PHP_EOL;
-echo 'type: ' . getClientType($browserInfo) . PHP_EOL;
-$clientApps = getClientApps($appTypes, $browserInfo);
-$sortedApps = sortPopularity($clientApps, $priorities, $browserInfo);
-$allApps = array_keys($appTypes);
-$finalApps = array_values(array_unique(array_merge($sortedApps, $allApps)));
-$finalApps = array_values(array_diff($allApps, $sortedApps));
-echo json_encode($clientApps);
-echo json_encode($sortedApps);
-echo json_encode($clientApps);
-echo json_encode($finalApps);
-
-
-echo $tpl->render(array(
-    'link' => '/android/',
-    'type' => 'android-mobile',
-    'image' => 'app_android_mobile.png',
-    'icon' => 'ic_android_primary.svg',
-    'name' => 'AniLibria',
-    'platform' => 'Android'
-));
+$clientAppKeys = getClientApps($appTypes, $browserInfo);
+$sortedAppsKeys = sortByPriority($clientAppKeys, $priorities, $browserInfo);
+$allAppKeys = array_keys($appTypes);
+$otherAppsKeys = array_values(array_diff($allAppKeys, $sortedAppsKeys));
 
 ?>
 
 <main class='main clearfix'>
+
+    <?php
+    foreach ($sortedAppsKeys as $appKey) {
+        echo $tpl->render($appsData[$appKey]);
+    }
+    ?>
+    <?php
+    foreach ($otherAppsKeys as $appKey) {
+        echo $tpl->render($appsData[$appKey]);
+    }
+    ?>
 
 </main>

5b555d87ba5d4b0e1e3b4272291a2f6fe10d1036
Evgeny Nizamiev <radiationx@yandex.ru>
2020-08-22 12:32:48 +0500

    some app-detail changes

--- www/public/detail-cross.php
+++ www/public/detail-cross.php
@@ -1,6 +1,8 @@
 <?
     require dirname(__DIR__) . '/vendor/autoload.php';
+
     use app\common\Utils;
+
 ?>
 
 
@@ -13,7 +15,7 @@
     <?php Utils::includeCss("/styles/style.css"); ?>
 </head>
 
-<body>
+<body class="debugs">
 <header class="header">
     <ul class="navbar">
         <li class="bottom_line active"><a href="#">Приложения</a></li>
@@ -30,28 +32,37 @@
 
 <main class="main detail clearfix">
     <div class="image_wrapper">
-        <img src="res/images/app_android_mobile.png" class="app_image">
+        <img src="res/images/app_cross_anilibrix.png" class="app_image" alt="anilibrix">
     </div>
-    <div class="app_name">AniLibria Android</div>
+    <div class="app_name">AniLibrix</div>
     <div class="app_desc">Просто чоткая аппка</div>
     <div class="center_wrapper">
-        <a href="#" class="btn_download filled"><img src="res/icons/ic_android_white.svg" alt="" class="app_icon">Скачать
-            приложение</a>
-        <br>
-        <a href="#" class="btn_download small"><img src="res/icons/ic_github_primary.svg" alt="" class="app_icon">Исходный
-            код</a>
+        <div class="app_mod">
+            <a href="#" class="btn_download filled">
+                <img src="res/icons/ic_apple_white.svg" alt="" class="app_icon">
+                <span>Скачать для macOS</span>
+            </a>
+
+
+        </div>
+        <div class="app_mod">
+            <a href="#" class="btn_download filled">
+                <img src="res/icons/ic_windows_white.svg" alt="" class="app_icon">
+                <span>Скачать для Windows</span>
+            </a>
+        </div>
+        <div class="app_mod">
+            <a href="#" class="btn_download filled">
+                <img src="res/icons/ic_linux_white.svg" alt="" class="app_icon">
+                <span>Скачать для Linux x64</span>
+            </a>
+        </div>
     </div>
-    <div id="how_install" class="text_block  clearfix">
-        <h3>Как установить</h3>
-        <p style="text-align: left;">
-            - Запустите загруженный файл<br>
-            - Вы увидите окно с надписью <u>Установка заблокирована</u>, не пугайтесь, нажмите кнопку
-            <u>настройки</u>.<br>
-            - Найдите пункт <u>Неизвестные источники</u> и выставьте параметр <u>разрешить</u>.<br>
-            - Выберите галочку <u>Разрешить только эту установку</u> и нажмите <u>ок</u>.<br>
-            - Нажмите установить, когда появится надпись <u>приложение установлено</u>, нажмите <u>открыть</u>.<br>
-            - Исходный код доступен на <a href="https://github.com/anilibria/anilibria-app" target="_blank">github</a>.
-        </p>
+    <div class="center_wrapper">
+        <a href="#" class="btn_download small sub">
+            <img src="res/icons/ic_github_primary.svg" alt="" class="app_icon">
+            Исходный код
+        </a>
     </div>
 </main>
 </body>

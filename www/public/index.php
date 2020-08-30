2179c73304d4aba1b66499066045428cbc24febe
Evgeny Nizamiev <radiationx@yandex.ru>
2020-08-30 11:51:22 +0500

    add favicon

--- www/public/index.php
+++ www/public/index.php
@@ -23,6 +23,19 @@
 <head>
     <meta charset="UTF-8">
     <title>AniLibria - Приложения</title>
+
+    <!-- #Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/res/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/res/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/res/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/res/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/res/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="/res/favicon/favicon.ico">
+    <meta name="msapplication-TileColor" content="#ba3c41">
+    <meta name="msapplication-config" content="/res/favicon/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    <!-- /Favicon -->
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php Utils::includeCss("/styles/style.css"); ?>
 </head>
